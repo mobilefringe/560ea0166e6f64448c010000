c6c77cae7c19c2b8580027973e3f5225e5cf0cc0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-22 16:31:58 -0400

    Les Colonnades Point-Claire 1.2  /application.js

--- application.js
+++ application.js
@@ -284,11 +284,11 @@ $(document).ready(function() {
 	//Using i18n for localization, for more info please visit //i18next.com/
 	i18n.init({preload: [getStorage().primary_locale, getStorage().secondary_locale], resGetPath: '../__lng__.json', fallbackLng: false }, function(t) {
 			var current_locale = Cookies.get('primary_locale');
-        if(typeof(Cookies.get('current_locale')) != 'undefined' ){
-            current_locale = Cookies.get('current_locale')
-        }
-        // We're switching french to be the primary locale     
-        if(current_locale == Cookies.get('primary_locale')){
+            if(typeof(Cookies.get('current_locale')) != 'undefined' ){
+                current_locale = Cookies.get('current_locale')
+            }
+            // We're switching french to be the primary locale     
+            if(current_locale == Cookies.get('primary_locale')){
 				setPrimaryLanguage();
 				$('.en-CA').addClass('langSelect');
 				$('.fr-CA').removeClass('langSelect');
